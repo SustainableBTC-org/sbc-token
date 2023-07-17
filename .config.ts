f2e6f4b0a12ff5c5b88fc61b6ae569127eea95fc
baxy <sabau.adi@gmail.com>
2023-07-17 15:10:21 +0300

    Small config fix

--- .config.ts
+++ .config.ts
@@ -5,8 +5,8 @@ const config = {
   ethSepoliaUrl: process.env.ETH_SEPOLIA_URL || '',
   ethSepoliaOwnerPrivateKey: process.env.ETH_SEPOLIA_OWNER_PRVATE_KEY || '',
 
-  ethMainnetUrl: process.env.ETH_SEPOLIA_OWNER_PRVATE_KEY || '',
-  ethMainnetOwnerPrivateKey: process.env.ETH_SEPOLIA_OWNER_PRVATE_KEY || '',
+  ethMainnetUrl: process.env.ETH_MAINNET_URL || '',
+  ethMainnetOwnerPrivateKey: process.env.ETH_MAINNET_OWNER_PRVATE_KEY || '',
 
   sbcTokenContractAddress: process.env.SBC_TOKEN_CONTRACT_ADDRESS || '',
   mintToAccountAddress: process.env.MINT_TO_ACCOUNT_ADDRESS || '',
