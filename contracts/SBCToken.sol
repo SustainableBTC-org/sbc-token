06fd4e473df3b1d0240f016e9f4cf4e1b56ca50c
baxy <sabau.adi@gmail.com>
2024-07-17 21:59:01 +0300

    Add Permit

--- contracts/SBCToken.sol
+++ contracts/SBCToken.sol
@@ -3,9 +3,10 @@ pragma solidity ^0.8.23;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 import "@openzeppelin/contracts/utils/structs/EnumerableMap.sol";
 
-contract SBCToken is Ownable, ERC20Capped {
+contract SBCToken is Ownable, ERC20Capped, ERC20Permit {
     mapping (uint256 => uint256) public tokensMintedAtBtcBlockHeight;
     uint256[] public btcBlockHeights;
 
@@ -13,6 +14,7 @@ contract SBCToken is Ownable, ERC20Capped {
         ERC20("Sustainable Bitcoin Certificate", "SBC")
         ERC20Capped(21000000 * 10 ** 8)
         Ownable(initialOwner)
+        ERC20Permit("Sustainable Bitcoin Certificate")
     {}
 
     function decimals() override public pure returns (uint8) {
@@ -36,5 +38,9 @@ contract SBCToken is Ownable, ERC20Capped {
 
     function burn(uint256 value) public onlyOwner {
         _burn(_msgSender(), value);
+    }
+
+    function _update(address from, address to, uint256 value) internal virtual override(ERC20, ERC20Capped) {
+        super._update(from, to, value);
     }
 }
